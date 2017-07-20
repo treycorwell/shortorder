8fed8bc1c664999da644ad8dd0068cf9640874cf
Anne Meadows <ameadows@k12.wv.us>
2017-07-20 14:03:58 -0400

    removed hidden fields no longer needed

--- public/js/getdate.js
+++ public/js/getdate.js
@@ -1,5 +1,3 @@
 	function getOrderDate(id) {
-		var date = Date();
-		var dateAsString = date.toString();
-		document.getElementById(id).value = dateAsString;
+		document.getElementById(id).value = Date();
 	}
